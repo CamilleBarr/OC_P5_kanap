cfb04af5a1baab606971f1c752f57979178969c5
Camille <84985836+CamilleBarr@users.noreply.github.com>
2022-04-19 10:54:51 +0200

    organisation du code : boucle et creation d'élément intégré dans la function product

--- front/js/script2.js
+++ front/js/script2.js
@@ -5,98 +5,158 @@ fetch('http://localhost:3000/api/products')
         return res.json()
     }
 })
-.then(function(value){
-    console.log (value);
+.then(function(products){ // j'appelle le tableau et récupère les données du tableau
+    
+    console.log (products); // je vérifie que le tableau est bien intégré
+    
+    const sectionElt = document.getElementById("items"); // la constance correspondra dans le HTML à la section dont l'ID est items
+    
+    for (let product of products){ // je créé une boucle qui dit que chaque Items/objects/product dans le tableau/webapi products 
+        
+        console.log (product) // on vérifie d'abord que les items/objects/product ooient bien récupérés
+        
+        // création des éléments HTML 
+
+            //1. lien du produit / Card
+
+            let linkElt = document.createElement("a");
+
+            sectionElt.appendChild(linkElt);
+
+            // 2. article
+            let articleElt = document.createElement('article');
+
+            linkElt.appendChild(articleElt);
+
+            //3. img
+            let imageElt = document.createElement('img');
+
+            articleElt.appendChild(imageElt);
+            imageElt.classList.add('imageUrl')
+            //4. h3 et creation de class
+            let h3Elt = document.createElement('h3');
+
+            h3Elt.innerHTML = product.name
+
+            articleElt.appendChild(h3Elt);
+            h3Elt.classList.add('name');
+
+            //5. p et creation de class
+            let pElt = document.createElement('p');
+
+            articleElt.appendChild(pElt);
+            pElt.classList.add('altText');
+
+    };
 })
 .catch(function(err)
 {
 })
-// création des éléments HTML 
-//1. lien du produit / Card
-
-const linkElt = document.createElement("a");
-let sectionElt = document.getElementById("items");
-
-sectionElt.appendChild(linkElt);
-
-// 2. article
-const articleElt = document.createElement('article');
-
-linkElt.appendChild(articleElt);
-
-//3. img
-const imageElt = document.createElement('img');
-
-articleElt.appendChild(imageElt);
-//4. h3
-const h3Elt = document.createElement('h3');
-
-articleElt.appendChild(h3Elt);
-h3Elt.classList.add('productName');
-
-//5. p
-const pElt = document.createElement('p');
-
-articleElt.appendChild(pElt);
-pElt.classList.add('productDescription');
 
 // récupération du contenu du tableau
-const items = document.getElementsByTagName('items');
-const firstItem = items[0];
-const secondtItem = items[1];
-const thirdItem = items[2];
-const fourthItem = items[3];
-const fifthItem = items[4];
-const sixthItem = items[5];
-const seventhItem = items[6];
-const eighthItem = items[7];
 
-//Constance pour ajouter dans le DOM un lien
-// 
-     
+/*let products=['object1','object2']
+console.log(products[0]);
+console.log(products[1]);
+//n'affiche rien, juste les creation d'élément, base d'une card
+*/
+/*
+const products = 8;
 
-    //const a = document.getElementsByName('imageURL')
+for (let i=0; i<10; i++);
+//console.log(products);
+//affiche qu'une card vierge*/
 
-//     const img = document.createElement('<img>');
-//     article.appendChild(img); 
+/*
+let products = []
 
-const numberOfProductByPage = 8;
-for (let i = 0; i < numberOfProductByPage; i++) {
-//console.log('Object'[0,'...','Object'[1]);
 
-}// Csq 11 message
+}
+*/
+/*
+let products = [ object2];
+for (let i in products){
+console.log (products [i]);
+}
+// affiche une card vierge et dans la console affiche 1 et 2.
+*/
+/*
+let products = ['object'[1], 'object'[2]];
+for (let i in products){
+console.log (products [i]);
+}
+// s'affiche une card vierge et dans la console on voit b et j.
+*/
+
+/*const product = [
+    {imageUrl
+    }
+    
+]*/
+
+/*for (let products of product) {
+     console.log([]);
+ }
+ */
+
+/*let products=["index[1]", "index[2]"];
+let allProducts=products.length;
+*/
+
+// il faut associer altText au paragraphe, associer name à h3 et associer imageUrl 
+
+/*    let products = 8;
+for (let i = 0; i < products; i++) {
+   console.log(items[i]);
+}    
+//Csq 9 messages
+*/
+/*
+products = []
+for (let i in products) {
+console.log(products[i]);
+}
+*/
+
+//elementList = parentNode.querySelectorAll('imageUrl','name', 'altText');
+
+
+//let products = document.getElementsByArray(products);
+
+/*
+const firstItem = products[0];
+const secondtItem = products[1];
+const thirdItem = products[2];
+const fourthItem = products[3];
+const fifthItem = products[4];
+const sixthItem = products[5];
+const seventhItem = products[6];
+const eighthItem = products[7];
+*/
+
+// Csq 11 message
 /*  const numberOfProductByPage = 9;
 for (let i = 0; i < numberOfProductByPage; i++) {
    console.log(items[i]);
 }
 // Csq rien
 */   
-/*    const numberOfProductByPage = 8;
-for (let i = 0; i < numberOfProductByPage; i++) {
-   console.log(items[i]);
-}    
-//Csq 9 messages
+
+
+
+
+
+/*
+ //link between the display of all products and the product page itself
+ document
+   .getElementById("items")
+   .addEventListener("click", items);
 */
-/*for (let i in items) {
-console.log(items[i]);
-}
-//csq 307 messages d'infos
+/*     class productItem {
+     constructor (imageUrl, name, altTxt)
+         {   this.imageUrl='products[].imgUrl',
+             this.name='products[].name',
+             this.altTxt='products[].altTxt',
+         }
+     };
 */
-
-// link between the display of all products and the product page itself
-// document
-//   .getElementById("items")
-//   .addEventListener("click", items);
-
-
-
-
-
-    // class productItem 
-    // {constructor ()
-    //     {   this.name='array[].name',
-    //         this._id='array[]._id',
-    //         this.img='array[].img',
-    //         this.description='array[].description'
-    //     }
-    // };
