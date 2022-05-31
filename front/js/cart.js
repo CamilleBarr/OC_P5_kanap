a86fb05568067fdc4b67f2e74283cf00679bffa1
Camille <84985836+CamilleBarr@users.noreply.github.com>
2022-05-31 20:36:11 +0200

    manipulation du DOM mis en fonction à part

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,10 +1,14 @@
 /* 
+BUG : 
+afficher un prix décimal avec 2 chiffres après la virgule
+afficher le text couleur plutôt qu'un chiffre
+
 7. je créé un évènement au clic "confirmation" avec localStorage etc.
 PARTIE 3
 6. je mets des conditions regex au formulaire
 PARTIE 2
 5. Je créé un calcul sur totalQuantity et totalPrice
-4. je créé un évènement au clic "suppresion" du produit
+4. je créé un évènement au clic "suppression" du produit
 4. je créé une condition si doublon de produit
 3. je récupère les données de l'API directement pour le prix, src et text image
 2. j'affiche le localstorage en fonction du contenu
@@ -16,23 +20,18 @@ let tableauKanap = JSON.parse(localStorage.getItem('listOfProduct'));
 console.log(tableauKanap);
 
 //en fonction du retour consolelog faire boucle
-
+//condition, si produit aux caractéristiques similaires = fusionner et additionner la quantité,
+//sinon, new cart(kanap)
   for (let kanap of tableauKanap) {
     cart(kanap)
   }
 
+
+
 //localStorage.setItem('listOfProduct', JSON.stringify(listOfProduct));
 //1//
-function cart(kanap) {
-  fetch('http://localhost:3000/api/products/' + kanap.id)
-    .then(function (res) {
-      if (res.ok) {
-        console.log('res', res);
-        return res.json()
-      }
-    })
-    .then(function (res) {
-      let cartSection = document.getElementById('cart__items');
+const createHTMLContent = (res, kanap) => {
+  let cartSection = document.getElementById('cart__items');
       let cartArticle = document.createElement('article');
     
       cartSection.appendChild(cartArticle);
@@ -66,7 +65,7 @@ function cart(kanap) {
       description.appendChild(pColor);
     
       let pPrice = document.createElement('p');
-      pPrice.innerHTML = (res.price + " €"); //récupérer le prix du produit de l'API
+      pPrice.innerHTML = (res.price + ",00 €"); //récupérer le prix du produit de l'API
       description.appendChild(pPrice);
     
       let settings = document.createElement('div');
@@ -98,49 +97,26 @@ function cart(kanap) {
       pDelete.className = 'deleteItem';
       settingsDelete.appendChild(pDelete);
       pDelete.innerHTML = "Supprimer";
+}
 
+function cart(kanap) {
+  console.log('kanap', kanap);
+  fetch('http://localhost:3000/api/products/' + kanap.id)
+    .then(function (res) {
+      if (res.ok) {
+        console.log('res', res);
+        return res.json()
+      }
+    })
+    .then(function (res) {
+      createHTMLContent(res, kanap); 
     })
     .catch(function (err) {
-      console.log(err, "erreur")
+      console.log("erreur", err)
     })
 };
 
-/*  ce que je dois créer pour intégrer mes données : 
 
-        <!--  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
-        <div class="cart__item__img">
-          <img src="../images/product01.jpg" alt="Photographie d'un canapé">
-        </div>
-        <div class="cart__item__content">
-          <div class="cart__item__content__description">
-            <h2>Nom du produit</h2>
-            <p>Vert</p>
-            <p>42,00 €</p>
-          </div>
-          <div class="cart__item__content__settings">
-            <div class="cart__item__content__settings__quantity">
-              <p>Qté : </p>
-              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-            </div>
-            <div class="cart__item__content__settings__delete">
-              <p class="deleteItem">Supprimer</p>
-            </div>
-          </div>
-        </div>
-      </article> -->
-*/
-
-/*
-let removeFromCart = (product) => {
-
-  let minusQuantity = document.getElementsByClassName('deleteItem');
-  for (minusQuantity of minusQuantity) {
-    itemQuantity.delete(minusQuantity)
-  }
-  console.log(minusQuantity);
-
-}
-*/
 /*
   let cart__items = getToCart();
   cart__items = cart__items.filter(p => p.id != product.id);
