fe18a78fada34cd0c93145bbd9d2c76d75c7ec0a
Camille <84985836+CamilleBarr@users.noreply.github.com>
2022-07-12 15:32:57 +0200

    link url set in var and mainfunction created

--- front/js/script.js
+++ front/js/script.js
@@ -1,12 +1,6 @@
-//Function "res" permet d'assurer la bonne récupération de l'API products
-fetch('http://localhost:3000/api/products')
-.then(function(res){        
-    if (res.ok){
-        return res.json()
-    }
-})
-//Promesse / function "product" permet de créer l'architecture de la page et y intégrer les données de l'API
-.then(function(products){
+var url = 'http://localhost:3000/api/products';
+
+let mainFunction =(products) => {
     const sectionElt = document.getElementById("items");
     for (let product of products){
         let linkElt = document.createElement("a"); 
@@ -25,8 +19,17 @@ fetch('http://localhost:3000/api/products')
         pElt.innerHTML = product.altTxt;
         articleElt.appendChild(pElt); 
     };
+};
+
+fetch(url)
+.then(function(res){        
+    if (res.ok){
+        return res.json()
+    }
 })
-//Promesse / Function catch permet de nous renvoyer une erreur en place de la promesse res et affichage de l'architecture produit
+.then(function(products){
+    mainFunction(products)
+})
 .catch(function(err)
 {
     console.log(err, "erreur")
